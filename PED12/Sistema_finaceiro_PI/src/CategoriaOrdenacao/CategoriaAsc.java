c102de425712d7ee0da8289e4a149011cf33dd29
Davi Martins <davimartins.dgcm@gmail.com>
2021-09-06 13:44:54 -0300

    Ordenação das categorias

---
+++ PED12/Sistema_finaceiro_PI/src/CategoriaOrdenacao/CategoriaAsc.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CategoriaOrdenacao;
+
+import java.util.Comparator;
+// falta a importação de categoria
+import java.text.Collator;
+
+/*
+
+public class CategoriaAsc implements Comparator<Categoria> {
+     public CategoriaAsc() {
+    
+     }
+     
+    @Override
+    public int compare(Categoria c, Categoria c1) {
+
+        // c é maior que c1
+        if (Collator.getInstance().compare(c.getCategoria_aux(), c1.getCategoria_aux()) > 0) {
+            return 1;
+            
+        } else if (Collator.getInstance().compare(c.getCategoria_aux(), c1.getCategoria_aux()) < 0) {
+             // c é menor que c1
+            return -1;
+
+        } else {
+              // são iguais
+            return 0;
+        }
+    
+
+    }
+}
+*/
